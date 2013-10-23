0c27a977713307af1bc098aa1f83aea948df23ba
IMAMASU Ryohei <imms.ryohei@gmail.com>
2013-10-23 14:13:00 +0900

    Add default query type

--- common/default.h
+++ common/default.h
@@ -9,7 +9,7 @@
 #define OPT_DEFAULT_TIMEOUT_SEC                 10000
 #define OPT_DEFAULT_DATASET_PATH                ""
 #define OPT_DEFAULT_DATASET_LIMIT               -1
-#define OPT_DEFAULT_QUERY_MODE                  ""
+#define OPT_DEFAULT_QUERY_MODE                  "update"
 #define OPT_DEFAULT_BULK_SIZE                   1
 #define OPT_DEFAULT_THREAD_NUM                  1
 #define OPT_DEFAULT_QUERY_NUM_PER_THREAD        0
